7d60df9cea96e172db71839cef42f9a703a07838
HIMANSHUYELANE <yelanehimanshu@gmail.com>
2025-01-08 22:17:26 +0530

    edit functionality

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
+const { title } = require('process');
+const { render } = require('ejs');
 
 app.set("view engine",'ejs');
 app.use(express.json());
@@ -20,16 +22,21 @@ app.get('/files/:filename',function(req, res){
     })
 })
 
-app.get('/edit',function(req,res){
-    res.render('edit')
+app.get('/edit/:filename',function(req,res){
+    res.render('edit',{title:req.params.filename})
 })
 
 app.post('/create',function(req, res){
-    console.log(req.body)
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.desc ,function(err){
         res.redirect("/")
     })
+})
+
+app.post("/edit", function(req, res){
+    fs.rename(`./files/${req.body.prvs}`,`./files/${req.body.new}`,function(err){
+        res.redirect("/")
     })
+})
 
 
 app.listen(3000,function(){
